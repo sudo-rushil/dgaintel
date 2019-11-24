fac2ff1fe69f235d7b36bae297dba3c734e04648
sudo-rushil <rushil.mallarapu@gmail.com>
2019-11-24 16:12:43 -0500

    release v0.3

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
   name = 'dgaintel',         # How you named your package folder (MyLib)
   packages = ['dgaintel'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'predict whether a domain is genuine or DGA.',   # Give a short description about your library
   author = 'Rushil Mallarapu',                   # Type in your name
   author_email = 'rushil.mallarapu@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/sudo-rushil/dgaintel',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/sudo-rushil/dgaintel/archive/v0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/sudo-rushil/dgaintel/archive/v0.2.tar.gz',    # I explain this later on
   keywords = ['DGA', 'Domain', 'Domain Generation', 'Domain Classifier', 'Deep Learning', 'AI', 'RNN', 'LSTM', 'CNN-LSTM', 'CNN'],
-  include_package_data=True
+  include_package_data=True,
   install_requires=[            # I get to this in a second
           'numpy',
           'tensorflow',
