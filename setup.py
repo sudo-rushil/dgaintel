56261bf228486ddff502ae08270cab3b33e85315
sudo-rushil <rushil.mallarapu@gmail.com>
2019-11-25 16:22:18 -0500

    added PyPI description

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 
 from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
   name = 'dgaintel',         # How you named your package folder (MyLib)
   packages = ['dgaintel'],   # Chose the same as "name"
-  version = '0.7',      # Start with a small number and increase it with every change you make
+  version = '0.8',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Use deep learning to predict whether a domain name is genuine or DGA.',   # Give a short description about your library
+  long_description = long_description,
+  long_description_content_type='text/markdown',
   author = 'Rushil Mallarapu',                   # Type in your name
   author_email = 'rushil.mallarapu@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/sudo-rushil/dgaintel',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/sudo-rushil/dgaintel/archive/v0.7.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/sudo-rushil/dgaintel/archive/v0.8.tar.gz',    # I explain this later on
   keywords = ['DGA', 'Domain', 'Domain Generation', 'Domain Classifier', 'Deep Learning', 'AI', 'RNN', 'LSTM', 'CNN-LSTM', 'CNN'],
   include_package_data=True,
   install_requires=[            # I get to this in a second
